05a0f6a9abd482c2c6e3e889c78a9c51f23664c1
SofiaSchneider-devbox <sofia.schneider@devbox.com.mx>
2024-10-15 15:42:21 -0600

    Initial Commit

---
+++ Explora-Papalote/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Explora-Papalote
+//
+//  Created by Sofia Schneider on 10/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Explora-Papalote/Explora_PapaloteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Explora_PapaloteApp.swift
+//  Explora-Papalote
+//
+//  Created by Sofia Schneider on 10/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Explora_PapaloteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
