8046261f03c1907b71073f2d181cc3e27d4f83e2
SofiaSchneider-dev <sofiaschneiderj02@gmail.com>
2024-10-15 16:47:13 -0600

    ImagenPlantaAlta

---
+++ ElegirModoView.swift
@@ -0,0 +1,18 @@
+//
+//  ElegirModoView.swift
+//  Explora-Papalote
+//
+//  Created by Sofia Schneider on 10/15/24.
+//
+
+import SwiftUI
+
+struct ElegirModoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ElegirModoView()
+}
